7d691f9b2fc5d1e6a901f9ae4bfc8f1b763c705b
jhaworth21 <jhaworth21@gmail.com>
2021-05-17 00:31:26 +0100

    added the skeleton code for RamblersSearch, RamblersState and created RunRamblersBB

---
+++ code/search3/RamblersState.java
@@ -0,0 +1,24 @@
+public class RamblersState extends SearchState{
+
+    public int getLocalCost() {
+        //TODO check worksheet and complete function - getLocalCost
+        return localCost;
+      }
+    
+    ArrayList<SearchState> getSuccessors(Search searcher){
+        //TODO check worksheet and complete function - getSuccessors
+
+    }
+
+    boolean goalPredicate(Search searcher) {
+        //TODO check worksheet and complete function - goalPredicate
+
+        return false;
+    }
+
+    boolean sameState(SearchState n2) {
+        //TODO check worksheet and complete function - sameState
+
+        return false;
+    }
+}
