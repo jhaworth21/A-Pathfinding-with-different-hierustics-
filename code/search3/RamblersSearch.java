475894015bd6f041f5f744cbb7f2c6ec244abeba
jhaworth21 <jhaworth21@gmail.com>
2021-05-17 16:35:51 +0100

    added the methods for RamblersSearch

--- code/search3/RamblersSearch.java
+++ code/search3/RamblersSearch.java
@@ -1,4 +1,19 @@
 public class RamblersSearch extends Search{
 
+    private TerrainMap map;
+    private Coords goal; 
+
+    public TerrainMap getMap() {
+        return map;
+    }
     
+    public Coords getGoal(){
+        return goal;
+    }
+
+    public RamblersSearch(TerrainMap m, Coords g){
+        map = m;
+        goal = g;
+    }
+
 }
--- code/search3/RamblersState.java
+++ code/search3/RamblersState.java
@@ -1,5 +1,6 @@
 public class RamblersState extends SearchState{
 
+    
     public int getLocalCost() {
         //TODO check worksheet and complete function - getLocalCost
         return localCost;
@@ -7,18 +8,15 @@ public class RamblersState extends SearchState{
     
     ArrayList<SearchState> getSuccessors(Search searcher){
         //TODO check worksheet and complete function - getSuccessors
-
     }
 
     boolean goalPredicate(Search searcher) {
         //TODO check worksheet and complete function - goalPredicate
-
         return false;
     }
 
     boolean sameState(SearchState n2) {
         //TODO check worksheet and complete function - sameState
-
         return false;
     }
 }
